a9b8264575c8b06e14d308c5b5b0b725009b0dd2
maryplank <kaleidoscopesland@hotmail.com>
2019-06-21 15:44:32 -0300

    venv created

---
+++ venv/Lib/site-packages/cs50/__init__.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+try:
+
+    # Save student's sys.path
+    path = sys.path[:]
+
+    # In case student has files that shadow packages
+    sys.path = [p for p in sys.path if p not in ("", os.getcwd())]
+
+    # Import cs50_*
+    from .cs50 import get_char, get_float, get_int, get_string
+    try:
+        from .cs50 import get_long
+    except ImportError:
+        pass
+
+    # Replace Flask's logger
+    from . import flask
+
+    # Wrap SQLAlchemy
+    from .sql import SQL
+
+finally:
+
+    # Restore student's sys.path (just in case library raised an exception that caller caught)
+    sys.path = path
